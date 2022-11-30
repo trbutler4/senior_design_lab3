04ebc610c061b672c954e742fd7c50014a07031f
Jordyn Iannuzzelli <jordyn-iannuzzelli@uiowa.edu>
2022-11-29 18:43:27 -0600

    jordyn

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,9 @@ import CommentSection from '../components/CommentSection'
 const IndexPage = () => {
   return (
   <Layout pageTitle="Geek Squad">
+      <br></br>
       <p>Welcome to our Senior Design Group Home Page!</p>
+      <br></br>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
         src="../images/Geek.jpg"
