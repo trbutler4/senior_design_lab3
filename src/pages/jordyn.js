dfe95c29eb6cbb3e5ba387b2c777fe9971b63ce4
mjbrom <mjbrom10@outlook.com>
2022-12-01 16:28:45 -0600

    you get a comment box, and YOU get a comment box

--- src/pages/jordyn.js
+++ src/pages/jordyn.js
@@ -1,42 +1,50 @@
-import * as React from 'react'
-import { Link } from 'gatsby'
-import { StaticImage } from 'gatsby-plugin-image'
-import Layout from '../components/layout'
-
+import * as React from "react";
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import Layout from "../components/layout";
+const commentBox = require("commentbox.io");
 
 const JordynPage = () => {
   return (
-    document.body.style.backgroundColor = "pink",
-    <Layout pageTitle="About Jordyn">
-      <br></br>
-      
-      <p>University of Iowa</p>
-      <p>Major: Computer Science and Engineering</p>
+    (document.body.style.backgroundColor = "pink"),
+    (
+      <Layout pageTitle="About Jordyn">
+        <br></br>
 
-      <a href="https://www.linkedin.com/in/jordyn-iannuzzelli/" target="_blank" rel="noopener noreferrer">linkedin</a>
-      <br></br>
-      <br></br>
+        <p>University of Iowa</p>
+        <p>Major: Computer Science and Engineering</p>
 
-      <StaticImage
-        alt="Picture of Jordyn"
-        src="../images/jordynpicture.jpg"
-      />
-      <br></br>
-      <br></br>
-      
-      <p>This is my dog Murphy</p>
-      <br></br>
-      <StaticImage
-        alt="Picture of Jordyn"
-        src="../images/murphy.jpg"
-      />
+        <a
+          href="https://www.linkedin.com/in/jordyn-iannuzzelli/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          linkedin
+        </a>
+        <br></br>
+        <br></br>
 
+        <StaticImage
+          alt="Picture of Jordyn"
+          src="../images/jordynpicture.jpg"
+        />
+        <br></br>
+        <br></br>
 
-      <div class="commentbox"></div>
-      <script src="https://unpkg.com/commentbox.io/dist/commentBox.min.js"></script>
-      <script>commentBox('5705371448508416-proj')</script>
-    </Layout>
-  )
-}
+        <p>This is my dog Murphy</p>
+        <br></br>
+        <StaticImage alt="Picture of Jordyn" src="../images/murphy.jpg" />
 
-export default JordynPage
+        <script src="https://unpkg.com/commentbox.io/dist/commentBox.min.js"></script>
+        <div className="commentbox" />
+        <script>
+          {window.addEventListener("load", function () {
+            commentBox("5705371448508416-proj");
+          })}
+        </script>
+      </Layout>
+    )
+  );
+};
+
+export default JordynPage;
--- src/pages/tommy.js
+++ src/pages/tommy.js
@@ -1,33 +1,40 @@
-import * as React from 'react'
-import { StaticImage } from 'gatsby-plugin-image'
-import Layout from '../components/layout'
-
+import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import Layout from "../components/layout";
+const commentBox = require("commentbox.io");
 const TommyPage = () => {
   return (
-    document.body.style.backgroundColor = "red",
-    <Layout pageTitle="About Tommy">
-      <p> University of Iowa 2023 Computer Science and Engineering</p>
-      <StaticImage
-        alt="Tommy with Bear"
-        src="../images/Tommypic.jpg"
-      />
-      <br></br>
-      
-      <p>My name is Tommy Subaric, I am from Carpentersville, IL. Some of my hobbies include: 
-        Watching movies, playing sports and doing anything outdoors like hiking, biking, 
-        skiing and traveling. I am also huge fan of the Chicago Bears and Chicago Cubs!
-      </p>
-      <br></br>
-      <p>I'm gonna make them an offer they can't refuse. -Don Vito Corleone</p>
-      <StaticImage
-        alt="Tommy Godfather Picture"
-        src="../images/Godfatha.jpg"
-      />
-      <div class="commentbox"></div>
-      <script src="https://unpkg.com/commentbox.io/dist/commentBox.min.js"></script>
-      <script>commentBox('5705371448508416-proj')</script>
-    </Layout>
-  )
-}
+    (document.body.style.backgroundColor = "red"),
+    (
+      <Layout pageTitle="About Tommy">
+        <p> University of Iowa 2023 Computer Science and Engineering</p>
+        <StaticImage alt="Tommy with Bear" src="../images/Tommypic.jpg" />
+        <br></br>
 
-export default TommyPage
+        <p>
+          My name is Tommy Subaric, I am from Carpentersville, IL. Some of my
+          hobbies include: Watching movies, playing sports and doing anything
+          outdoors like hiking, biking, skiing and traveling. I am also huge fan
+          of the Chicago Bears and Chicago Cubs!
+        </p>
+        <br></br>
+        <p>
+          I'm gonna make them an offer they can't refuse. -Don Vito Corleone
+        </p>
+        <StaticImage
+          alt="Tommy Godfather Picture"
+          src="../images/Godfatha.jpg"
+        />
+        <script src="https://unpkg.com/commentbox.io/dist/commentBox.min.js"></script>
+        <div className="commentbox" />
+        <script>
+          {window.addEventListener("load", function () {
+            commentBox("5705371448508416-proj");
+          })}
+        </script>
+      </Layout>
+    )
+  );
+};
+
+export default TommyPage;
