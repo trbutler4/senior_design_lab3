f1cdfc529ea1cd3a81f3731a4106e1f8d2e90e94
mjbrom <mjbrom10@outlook.com>
2022-12-01 16:06:41 -0600

    Comment box showing up

--- src/pages/mark.js
+++ src/pages/mark.js
@@ -1,18 +1,30 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-
+import * as React from "react";
+import Layout from "../components/layout";
+const commentBox = require("commentbox.io");
 const MarkPage = () => {
+  // commentBox("5705371448508416-proj");
   return (
-    document.body.style.backgroundColor = "yellow",
-    <Layout pageTitle="About Mark">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    (document.body.style.backgroundColor = "yellow"),
+    (
+      <Layout pageTitle="About Mark">
+        <p>
+          Hi there! I'm the proud creator of this site, which I built with
+          Gatsby.
+        </p>
+        <script src="https://unpkg.com/commentbox.io/dist/commentBox.min.js"></script>
+        <div className="commentbox" />
+        <script>
+          {window.addEventListener("load", function () {
+            commentBox("5705371448508416-proj");
+          })}
+        </script>
 
-      
-      <div class="commentbox"></div>
-      <script src="https://unpkg.com/commentbox.io/dist/commentBox.min.js"></script>
-      <script>commentBox('5705371448508416-proj')</script>
-    </Layout>
-  )
-}
+        {/* <div class="commentbox"></div> */}
 
-export default MarkPage
+        {/* <script>commentBox('5705371448508416-proj')</script> */}
+      </Layout>
+    )
+  );
+};
+
+export default MarkPage;
