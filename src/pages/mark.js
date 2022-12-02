c36e2f2a7a231a7cc9dfcd2ba99c468c2ea08c29
mjbrom <mjbrom10@outlook.com>
2022-12-02 11:45:32 -0600

    New dog changes

--- src/pages/mark.js
+++ src/pages/mark.js
@@ -2,15 +2,36 @@ import React, { Component } from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import Layout from "../components/layout";
 import commentBox from "commentbox.io";
+import axios from "axios";
+import Button from "@mui/material/Button";
 
 class MarkPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dogImage: null,
+    };
+  }
+
   componentDidMount() {
     this.removeCommentBox = commentBox("5705371448508416-proj");
+    axios.get(`https://dog.ceo/api/breeds/image/random`).then((res) => {
+      const dogImage = res.data;
+      this.setState({ dogImage });
+    });
   }
 
   componentWillUnmount() {
     this.removeCommentBox();
   }
+
+  handleClick() {
+    axios.get(`https://dog.ceo/api/breeds/image/random`).then((res) => {
+      const dogImage = res.data;
+      this.setState({ dogImage });
+    });
+  }
+
   render() {
     return (
       (document.body.style.backgroundColor = "yellow"),
@@ -37,7 +58,15 @@ class MarkPage extends Component {
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
-          ></iframe>
+          />
+          <br />
+          <br />
+          <img src={this.state.dogImage?.message} alt="new" />
+          <br />
+
+          <Button variant="contained" onClick={() => this.handleClick()}>
+            New Dog!
+          </Button>
           <script src="https://unpkg.com/commentbox.io/dist/commentBox.min.js"></script>
           <div className="commentbox" />
           {/* <script>
